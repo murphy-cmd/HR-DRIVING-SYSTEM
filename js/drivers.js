f8524e70a088d41aac6ee393eb18c654429aaa8c
murphy-cmd <markjaysonmanzon514@gmail.com>
2026-06-16 18:06:51 +0800

    Add async function to load and display drivers

--- js/drivers.js
+++ js/drivers.js
@@ -1,1 +1,42 @@
+async function loadDrivers(){
 
+    const { data } =
+        await supabaseClient
+            .from("employees")
+            .select("*")
+            .eq(
+                "employee_type",
+                "driver"
+            );
+
+    let html = "";
+
+    data.forEach(driver => {
+
+        html += `
+        <tr>
+
+            <td>
+                ${driver.full_name}
+            </td>
+
+            <td>
+                ${driver.status}
+            </td>
+
+        </tr>
+        `;
+
+    });
+
+    document.getElementById(
+        "driverTable"
+    ).innerHTML = html;
+}
+
+loadDrivers();
+
+setInterval(
+    loadDrivers,
+    3000
+);
