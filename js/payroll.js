77eed14e05d170d4923d75d822f904016ea809de
murphy-cmd <markjaysonmanzon514@gmail.com>
2026-06-23 16:55:07 +0800

    Update payroll.js

--- js/payroll.js
+++ js/payroll.js
@@ -1,1 +1,71 @@
+async function generatePayroll() {
 
+    const { data, error } =
+        await supabaseClient
+            .from("attendance_daily")
+            .select("*")
+            .order(
+                "attendance_date",
+                {
+                    ascending: false
+                }
+            );
+
+    if (error) {
+
+        console.error(error);
+
+        return;
+    }
+
+    let html = "";
+
+    data.forEach(record => {
+
+        const dailyRate = 500;
+
+        const workHours =
+            parseInt(
+                record.work_hours
+            ) || 0;
+
+        const otHours =
+            parseInt(
+                record.ot_hours
+            ) || 0;
+
+        const hourlyRate =
+            dailyRate / 8;
+
+        const totalSalary =
+            (workHours * hourlyRate)
+            +
+            (otHours * hourlyRate * 1.25);
+
+        html += `
+
+        <tr>
+
+            <td>${record.employee_name}</td>
+
+            <td>${record.work_hours || "0h 0m"}</td>
+
+            <td>${record.ot_hours || "0h 0m"}</td>
+
+            <td>₱${dailyRate}</td>
+
+            <td>
+                ₱${totalSalary.toFixed(2)}
+            </td>
+
+        </tr>
+
+        `;
+    });
+
+    document.getElementById(
+        "payrollTable"
+    ).innerHTML = html;
+}
+
+generatePayroll();
