ca4af419846b3280514016a83c73a8b8210a95c3
wwwwwwwww259 <wakshakob@gmail.com>
2026-07-03 18:16:46 +0800

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,6 +1,9 @@
 const SUPABASE_URL = "https://odpvxqpkqsnzjpkgizeo.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_lK2ut9NJnOaZFJaZMDabmw_qSZxP6L6";
 
+console.log("window.db =", window.db);
+console.log("window.supabaseClient =", window.supabaseClient);
+
 const supabaseClient = supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
