4ca610b890be86a54a0510207b1eb93a33e0715a
murphy-cmd <markjaysonmanzon514@gmail.com>
2026-07-03 16:01:12 +0800

    Add newline at end of supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,5 +1,6 @@
 const SUPABASE_URL = "https://odpvxqpkqsnzjpkgizeo.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_lK2ut9NJnOaZFJaZMDabmw_qSZxP6L6";
+
 const supabaseClient = supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
