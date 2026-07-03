86060470200d76949067fa6f0f4d6ac53e133be4
murphy-cmd <markjaysonmanzon514@gmail.com>
2026-07-03 15:54:29 +0800

    Refactor Supabase client initialization

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,6 +1,10 @@
 const SUPABASE_URL = "https://odpvxqpkqsnzjpkgizeo.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_lK2ut9NJnOaZFJaZMDabmw_qSZxP6L6";
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
 
-window.db = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.db = supabaseClient;
 
 console.log("Supabase Ready");
