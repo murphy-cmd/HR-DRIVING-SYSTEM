06c849e572c9df5f515bf42e00bbf05d78b08ade
wwwwwwwww259 <wakshakob@gmail.com>
2026-07-08 15:24:22 +0800

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,1 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    const sidebar = document.querySelector(".sidebar");
+    const main = document.querySelector(".main");
+    const menuToggle = document.getElementById("menuToggle");
+
+    menuToggle.addEventListener("click", () => {
+
+        sidebar.classList.toggle("collapsed");
+        main.classList.toggle("expanded");
+
+    });
+
+});
